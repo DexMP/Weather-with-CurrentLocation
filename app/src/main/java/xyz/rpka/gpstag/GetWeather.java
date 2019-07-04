c0562b062bd6953b5e8a06f5356ebfb0c2579b15
DexMP <MatveyMP@yandex.ru>
2019-07-04 20:53:20 +0300

    Weather for current location

--- app/src/main/java/xyz/rpka/gpstag/GetWeather.java
+++ app/src/main/java/xyz/rpka/gpstag/GetWeather.java
@@ -1,4 +1,24 @@
 package xyz.rpka.gpstag;
 
 class GetWeather {
+    private float id;
+    private String main;
+    private String description;
+    private String icon;
+
+    public float getId() {
+        return id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
 }
